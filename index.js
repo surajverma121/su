c5ec7a893babf1aa3388bb0573191bdcc4ea9dbc
suraj verma <Surajverma7049214132@gmial.com>
2024-12-27 15:36:24 +0530

    Describe what you changed in index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(cors());
 app.use(express.json());
 
 console.log("Email User:", process.env.EMAIL_USER);
+
 console.log("Email Pass:", process.env.EMAIL_PASS);
 
 // Configure Multer for file uploads
